928759e714326935248548aaba0f0ada9a74b5d5
Micah Williamson <micahwllmsn@gmail.com>
2015-05-20 10:13:57 -0500

    alerts error messages

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,10 +26,11 @@ angular.module('donationsApp')
               }, followers);
 
               $scope.followers = followers;
-          }).error(function() {
+          }).error(function(err) {
               // called asynchronously if an error occurs
               // or server returns response with an error status.
               //TODO: use function(status) to alert status failure
+              alert('oh poop this happened:' + err);
           });
         } else {
           $scope.alertMessage = 'Please enter your Twitch Username';
